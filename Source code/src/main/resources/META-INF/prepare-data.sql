31ef04b25c35c5d69ce00efb0e201a5573a3542d
trung <trung@trungs-MacBook-Air.local>
2019-07-16 01:16:48 +0700

    prepare data sql

---
+++ Source code/src/main/resources/META-INF/prepare-data.sql
@@ -0,0 +1,6 @@
+insert into product(id,name) values (1,'Winner X');
+insert into product(id,name) values (2,'Ducati Panigale 1199');
+insert into product(id,name) values (3,'BMW S1000RR');
+insert into product(id,name) values (4,'Yamaha R1');
+insert into product(id,name) values (5,'CBR 1000R');
+insert into product(id,name) values (6,'Kawasaki Z1000');

833dad1ebc3c32dccd7f1bbacca0ad2c2c0977d0
trung <trung@trungs-MacBook-Air.local>
2019-07-16 01:10:21 +0700

    create entities

---
+++ Source code/src/main/java/entities/ProductEntity.java
@@ -0,0 +1,23 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="product")
+public class ProductEntity {
+	@Id
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+}
